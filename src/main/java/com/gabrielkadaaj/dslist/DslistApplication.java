d016bc24d9a0781c0fe021467d502050be97a052
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-09-01 15:21:29 -0300

    properties, Game, ORM

--- src/main/java/com/gabrielkadaaj/dslist/DslistApplication.java
+++ src/main/java/com/gabrielkadaaj/dslist/DslistApplication.java
@@ -2,11 +2,14 @@ package com.gabrielkadaaj.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.gabrielkadaaj.entities")
 public class DslistApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DslistApplication.class, args);
 	}
 
